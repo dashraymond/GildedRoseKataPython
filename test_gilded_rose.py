d98552eb83a864ed6cdc19e2ec3aac7c0d627b1f
Banerjee <banindo@amazon.com>
2025-02-15 11:39:33 -0800

    Added 4 failing tests

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,7 +5,7 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    # example of test that checks for logical errors
+    # test for logical errors
     def test_sulfuras_should_not_decrease_quality(self):
         items = [Item("Sulfuras", 5, 80)]
         gilded_rose = GildedRose(items)
@@ -15,7 +15,26 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEqual(4, sulfuras_item.sell_in)
         self.assertEqual("Sulfuras", sulfuras_item.name)
 
-    # example of test that checks for syntax errors
+    def test_item_quality_should_not_be_greater_than_50(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        Backstage_passes_item = items[0]
+        self.assertEqual(49, Backstage_passes_item.quality)
+        self.assertEqual(4, Backstage_passes_item.sell_in)
+        self.assertEqual("Backstage passes to a TAFKAL80ETC concert", Backstage_passes_item.name)
+        
+    def test_item_quality_should_not_be_negative(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        Backstage_passes_item = items[0]
+        self.assertEqual(-4, Backstage_passes_item.quality)
+        self.assertEqual(4, Backstage_passes_item.sell_in)
+        self.assertEqual("Backstage passes to a TAFKAL80ETC concert", Backstage_passes_item.name)     
+        
+
+    # test for syntax errors
     def test_gilded_rose_list_all_items(self):
         items = [Item("Sulfuras", 5, 80)]
         gilded_rose = GildedRose(items)
